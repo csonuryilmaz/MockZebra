a24afaf8f1e12fb9fbf7fe36f6166b40209e0da6
Onur Yılmaz <csonuryilmaz@gmail.com>
2018-10-24 23:04:09 +0300

    * Minlog library 's added. * Minlog 's customized with **MZLog** class. (MZ: MockZebra) * Version info 's initialized.

--- MockZebra/src/mockzebra/MockZebra.java
+++ MockZebra/src/mockzebra/MockZebra.java
@@ -1,23 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mockzebra;
 
-/**
- *
- * @author onur
- */
+import static com.esotericsoftware.minlog.Log.*;
+
 public class MockZebra
 {
 
-    /**
-     * @param args the command line arguments
-     */
+    private final static String VERSION = "0.0.0.0";
+
     public static void main(String[] args)
     {
-	// TODO code application logic here
+	setLogger(new MZLog());
+	info("VERSION", VERSION);
     }
 
 }
