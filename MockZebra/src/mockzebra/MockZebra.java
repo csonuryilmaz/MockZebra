f2bef31f820615d786a45b11fe492e6602b20c9e
Onur Yılmaz <csonuryilmaz@gmail.com>
2018-11-03 00:34:35 +0300

    Logo is added, help and version options are implemented.

--- MockZebra/src/mockzebra/MockZebra.java
+++ MockZebra/src/mockzebra/MockZebra.java
@@ -4,6 +4,7 @@ import static com.esotericsoftware.minlog.Log.*;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
@@ -11,9 +12,18 @@ public class MockZebra
 {
 
     private final static String VERSION = "1.0.0.0";
+    private final static String MOCKZEBRA = ""
+	    + "  __  __            _    _____    _                " + System.getProperty("line.separator")
+	    + " |  \\/  | ___   ___| | _|__  /___| |__  _ __ __ _  " + System.getProperty("line.separator")
+	    + " | |\\/| |/ _ \\ / __| |/ / / // _ | '_ \\| '__/ _` | " + System.getProperty("line.separator")
+	    + " | |  | | (_) | (__|   < / /|  __| |_) | | | (_| | " + System.getProperty("line.separator")
+	    + " |_|  |_|\\___/ \\___|_|\\_/____\\___|_.__/|_|  \\__,_| " + System.getProperty("line.separator")
+	    + "                                                  ";
 
     public static void main(String[] args)
     {
+	System.out.println(MOCKZEBRA);
+	System.out.println(System.getProperty("line.separator") + " " + VERSION + System.getProperty("line.separator"));
 	setLogger(new MZLog());
 	CommandLine cmdLine = getParser(getOptions(), args);
 	if (cmdLine.hasOption("help"))
@@ -56,14 +66,31 @@ public class MockZebra
 
     private static void help()
     {
-	warn("(@todo) help arg not implemented yet!");
-
+	HelpFormatter help = new HelpFormatter();
+	help.setNewLine(System.getProperty("line.separator"));
+	help.printHelp("MockZebra",
+		help.getNewLine()
+		+ "Mock Zebra Printer!" + help.getNewLine() + help.getNewLine()
+		+ "Test and view raw ZPL labels as png image or pdf document." + help.getNewLine()
+		+ "Simulates zebra printer working on socket connection." + help.getNewLine()
+		+ help.getNewLine(),
+		getOptions(),
+		help.getNewLine()
+		+ "Report bugs: csonuryilmaz@gmail.com" + help.getNewLine()
+		+ "Homepage: https://github.com/csonuryilmaz/MockZebra" + help.getNewLine()
+		+ "Releases: https://github.com/csonuryilmaz/MockZebra/releases" + help.getNewLine()
+		+ "Issues: https://github.com/csonuryilmaz/MockZebra/issues" + help.getNewLine()
+		+ help.getNewLine() + "Happy coding!" + help.getNewLine(),
+		 true);
 	System.exit(0);
     }
 
     private static void version()
     {
-	System.out.println(VERSION);
+	System.out.println("Copyright (c) 2018 Onur Yılmaz");
+	System.out.println("MIT License: <https://github.com/csonuryilmaz/MockZebra/blob/master/LICENSE>");
+	System.out.println("This is free software: you are free to change and redistribute it.");
+	System.out.println("There is NO WARRANTY, to the extent permitted by law.");
 	System.exit(0);
     }
 
